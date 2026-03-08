51a3c22fb816a888aa2f612839b51e772872a8bd
Asharalikhan4 <contactashar4@gmail.com>
2026-03-08 12:15:52 +0530

    React Navigation Configured

--- App.tsx
+++ App.tsx
@@ -6,35 +6,78 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+import TextInputScreen from './src/screens/TextInputScreen/TextInputScreen';
+import ScrollViewScreen from './src/screens/ScrollViewScreen/ScrollViewScreen';
+import FlatListScreen from './src/screens/ListScreen/FlatListScreen/FlatListScreen';
+import SectionListScreen from './src/screens/ListScreen/SectionListScreen/SectionListScreen';
+import PlatformSpecificCode from './src/screens/PlatformSpecificCode/PlatformSpecificCode';
+import ImagesScreen from './src/screens/ImagesScreen/ImagesScreen';
+import HandlingTouches from './src/screens/HandlingTouches/HandlingTouches';
+import Touchables from './src/screens/HandlingTouches/Touchables';
+import CustomFonts from './src/screens/CustomFonts/CustomFonts';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: {
+      screen: HomeScreen,
+      options: {
+        title: "Welcome"
+      }
+    },
+    TextInput: {
+      screen: TextInputScreen
+    },
+    ScrollView: {
+      screen: ScrollViewScreen
+    },
+    FlatList: {
+      screen: FlatListScreen
+    },
+    SectionList: {
+      screen: SectionListScreen
+    },
+    PlatformSpecificCode: {
+      screen: PlatformSpecificCode
+    },
+    Images: {
+      screen: ImagesScreen
+    },
+    HandlingTouches: {
+      screen: HandlingTouches
+    },
+    Touchables: {
+      screen: Touchables
+    },
+    CustomFonts: {
+      screen: CustomFonts
+    }
+  }
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Navigation />
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
